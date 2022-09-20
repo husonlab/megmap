cc7f77280d750dc4ee1e1624b7835e6ebdb9992b
anupam gautam <gautamanupam07@gmail.com>
2022-09-20 21:30:02 +0200

    Update

--- megmap/modules/collector.py
+++ megmap/modules/collector.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 from megmap.modules.utility import OutputDirectoryGenerator,tarDir,gb_to_mb
 from megmap.modules.diamondAlignment import DiamondAlignment
+from megmap.modules.HitToCount import FunctionalHitToCount
 
 class megmapProcessorClass:
 
@@ -25,7 +26,16 @@ class megmapProcessorClass:
     def Alignment(self)->str:
         
         if self.TakeTool == 'diamond':
-            DiamondAlignmentHandler=DiamondAlignment(self.TakeInputFile,self.TakeDatabase, self.TakePathOfDir, str(self.TakePrefix+".tab"), self.TakeToolPath, self.TakeIdentity, self.TakeAlignmentCoverage, self.TakeEvalue, self.TakeThreads, self.TakeRAM)
+            DiamondAlignmentHandler=DiamondAlignment(self.TakeInputFile,
+                                                    self.TakeDatabase,
+                                                    self.TakePathOfDir,
+                                                    str(self.TakePrefix+".tab"),
+                                                    self.TakeToolPath,
+                                                    self.TakeIdentity,
+                                                    self.TakeAlignmentCoverage,
+                                                    self.TakeEvalue,
+                                                    self.TakeThreads,
+                                                    self.TakeRAM)
 
             if not self.TakeToolPath: ##check if self.TakeToolPath variable is empty
                 AlignmentFileNameAndPath=DiamondAlignmentHandler.normalDIAlcommand()
@@ -33,7 +43,11 @@ class megmapProcessorClass:
                 AlignmentFileNameAndPath=DiamondAlignmentHandler.WithPathDIAlcommand()
             return(AlignmentFileNameAndPath)
 
-def megmapEntry(ReceiveInputFile: str ="", ReceiveOutput: str = "megmap", ReceiveDatabase: str = "", ReceiveTool: str ="", ReceiveToolPath: str="", ReceiveAlignmentIdentity: int=70, ReceiveAlignmentCoverage: int=70,ReceiveEvalue: str="", ReceivePrefix: str="",ReceiveThreads: int="",ReceiveRAM: str = "") -> None:
+def megmapEntry(ReceiveInputFile: str ="", ReceiveOutput: str = "megmap",
+                ReceiveDatabase: str = "", ReceiveTool: str ="",
+                ReceiveToolPath: str="", ReceiveAlignmentIdentity: int=70,
+                ReceiveAlignmentCoverage: int=70,ReceiveEvalue: str="",
+                ReceivePrefix: str="",ReceiveThreads: int="",ReceiveRAM: str = "") -> None:
 
     DirectoryCaller=OutputDirectoryGenerator(os.path.abspath(ReceiveOutput),'megmap')
     PathOfDir=DirectoryCaller.DicGenAndCheck()
@@ -50,6 +64,7 @@ def megmapEntry(ReceiveInputFile: str ="", ReceiveOutput: str = "megmap", Receiv
                                                         ReceivePrefix,
                                                         ReceiveThreads,
                                                         ReceiveRAM)
-    
+
     ReceiveAlignmentFileNameAndPath=megmapProcessorClassHandler.Alignment()
-    print(ReceiveAlignmentFileNameAndPath)
+    FunctionalHitToCount(TakeAlignmetnTabFile=ReceiveAlignmentFileNameAndPath,
+                         TakeThread=ReceiveThreads).ReadFileInMemory()
